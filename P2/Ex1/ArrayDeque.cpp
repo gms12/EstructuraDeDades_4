c58190f44acd65847a2edd6af34a7fba1f83cdfa
pedemonte96 <pedemonte96@users.noreply.github.com>
2018-03-17 18:00:59 +0100

    He implementat print, getFront i getRear
    Al resoldre problemes del projecte (tema Mac/Windows) ha canviat algunes coses, es possible que per treballar ho hagis de canviar tu tmb

--- P2/Ex1/ArrayDeque.cpp
+++ P2/Ex1/ArrayDeque.cpp
@@ -30,19 +30,19 @@ ArrayDeque::~ArrayDeque() {
 }
 
 bool ArrayDeque::isEmpty() {
-    return this->size==0;
+    return this->size == 0;
 }//comprovem que el comptador sigui 0
 
 bool ArrayDeque::isFull() {
-    return this->size==this->data.size();
+    return this->size == this->data.size();
 }//comprovem que el comptador sigui el mateix nombre que la mida de l'array
 
 void ArrayDeque::insertFront(int element) {
     if(!this->isFull()){
-        if(!this->isEmpty())front=(front-1)%data.size();
-        data[front]=element;
+        if(!this->isEmpty())front = (front-1)%data.size();
         /*en el cas del front tirem cap enrere per tal de mantenir-me sempre
          al davant.*/
+        data[front] = element;
         size++;
     }
     else throw invalid_argument("L'Array és plena.");
@@ -51,7 +51,7 @@ void ArrayDeque::insertFront(int element) {
 
 void ArrayDeque::insertRear(int element) {
     if(!this->isFull()){
-        if(!this->isEmpty())rear=(rear+1)%data.size();
+        if(!this->isEmpty())rear = (rear+1)%data.size();
         /*En el cas d'estar buida, front=rear=0. Inserir l'element davant o 
          darrera és indiferent.*/
         data[rear]=element;
@@ -62,13 +62,13 @@ void ArrayDeque::insertRear(int element) {
 
 void ArrayDeque::deleteFront() {
     if(!this->isEmpty()){
-        if(size==1){
-            front=0;
-            rear=0;
+        if(size == 1){
+            front = 0;
+            rear = 0;
         }
         /*en el cas de només tenir un element, portem els dos valors a 0,
          valors inicials.*/
-        else front=(front+1)%data.size();
+        else front = (front+1)%data.size();
         size--;
     }
     else throw invalid_argument("L'Array és buida.");
@@ -76,24 +76,37 @@ void ArrayDeque::deleteFront() {
 
 void ArrayDeque::deleteRear() {
     if(!this->isEmpty()){
-        if(size==1){
-            front=0;
-            rear=0;
+        if(size == 1){
+            front = 0;
+            rear = 0;
         }
-        else rear=(rear-1)%data.size();
+        else rear = (rear-1)%data.size();
         size--;
     }
     else throw invalid_argument("L'Array és buida.");
 }
 
 void ArrayDeque::print() {
-    
+    cout << "[";
+    for(int i = front; i<front+size; i++){
+        cout << this->data[(i)%data.size()];
+        if(i < front+size-1) cout << ", ";
+    }
+    cout << "]";
 }
 
 int ArrayDeque::getFront() {
-    return this->front;
+    if(!this->isEmpty()){
+        return this->data[front];
+    }
+    else throw invalid_argument("L'Array és buida.");
+    return 0;    
 }
 
 int ArrayDeque::getRear() {
-    return this->rear;
+    if(!this->isEmpty()){
+        return this->data[rear];
+    }
+    else throw invalid_argument("L'Array és buida.");
+    return 0;  
 }
