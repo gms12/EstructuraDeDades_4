9eceafaff1348683910d07130e41d52cc5594b6b
pedemonte96 <pedemonte96@users.noreply.github.com>
2018-03-17 18:16:59 +0100

    He acabat el main

--- P2/Ex1/main.cpp
+++ P2/Ex1/main.cpp
@@ -23,16 +23,60 @@ using namespace std;
 
 int menu(vector<string> a){
     int i;
-    for(i=0;i<a.size();i++){
-            cout<<i+1<<".\t"<<a[i]<<endl;
-        }
-        cin>>i;
-        i--;
-        if(i<0 or i>=a.size()){
-            cout<<"Opció incorrecte."<<endl;
-            return menu(a);
-        }
-        return i;
+    for(i = 0; i<a.size(); i++){
+        cout << i+1 << ".\t "<< a[i] << endl;
+    }
+    cin >> i;
+    i--;
+    if(i<0 || i >= a.size()){
+        cout<<"Opció incorrecte."<<endl;
+        return menu(a);
+    }
+    return i;
+}
+
+void insertFront(ArrayDeque &array){
+    try{
+        int element;
+        cout << "Introdueix l'element a inserir: ";
+        cin >> element;
+        array.insertFront(element);
+        cout << "Element " << element << " agregat pel davant." << endl;
+    }catch(invalid_argument& e){
+        cout<<e.what()<<endl;
+    }
+}
+
+void insertRear(ArrayDeque &array){
+    try{
+        int element;
+        cout << "Introdueix l'element a inserir: ";
+        cin >> element;
+        array.insertRear(element);
+        cout << "Element " << element << " agregat pel darrera." << endl;
+    }catch(invalid_argument& e){
+        cout<<e.what()<<endl;
+    }
+}
+
+void deleteFront(ArrayDeque &array){
+    try{
+        int element = array.getFront();
+        array.deleteFront();
+        cout << "Element " << element << " eliminat pel davant." << endl;
+    }catch(invalid_argument& e){
+        cout<<e.what()<<endl;
+    }
+}
+
+void deleteRear(ArrayDeque &array){
+    try{
+        int element = array.getRear();
+        array.deleteRear();
+        cout << "Element " << element << " eliminat pel darrera." << endl;
+    }catch(invalid_argument& e){
+        cout<<e.what()<<endl;
+    }
 }
 
 /*
@@ -58,11 +102,21 @@ int main(int argc, char** argv) {
             cout<<"Que vols fer?"<<endl;
             option=menu(vec_options);
             switch(option){
-                case 0:break;
-                case 1:break;
-                case 2:break;
-                case 3:break;
-                case 4:break;
+                case 0:
+                    insertFront(array);
+                    break;
+                case 1:
+                    insertRear(array);
+                    break;
+                case 2:
+                    deleteFront(array);
+                    break;
+                case 3:
+                    deleteRear(array);
+                    break;
+                case 4:
+                    array->print();
+                    break;
                 case 5:cout<<"Adéu!"<<endl;;
             }
         }while(option!=5);
