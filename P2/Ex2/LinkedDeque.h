d1ca9118d7c2c2a2bcf38115f5308396aec3e4b1
Gabriel Marin <gabriel.marin.sanchez.12@gmail.com>
2018-04-13 18:47:02 +0200

    Delete i get Rear i Front

--- P2/Ex2/LinkedDeque.h
+++ P2/Ex2/LinkedDeque.h
@@ -11,8 +11,10 @@
 
 #ifndef LINKEDDEQUE_H
 #define LINKEDDEQUE_H
+#include <stdexcept>
+#include <iostream>
 #include "Node.h"
-
+using namespace std;
 template <class Element> class LinkedDeque {
     public:
         LinkedDeque();
@@ -64,20 +66,46 @@ template <class Element> void LinkedDeque::insertFront(const Element& element){
 template <class Element> void LinkedDeque::insertRear(const Element& element){
     //TODO
 }
+//Si la llista no es buida, eliminem el node que apunta el front.
 template <class Element> void LinkedDeque::deleteFront(){
-    //TODO
+    if(this->isEmpty())throw invalid_argument("La cua és buida.");
+    else{
+        Node<Element> *_aux; //node auxiliar que apuntarà al node que volem eliminar
+        _aux=this->_front->getNext(); //assigmen el node auxiliar
+        _aux->getNext()->setPrevious(this->_front); //movem la "fletxa" que apunta el node 2 al node 1
+        this->_front->setNext(_aux->getNext()); //movem la "fletxa" del sentinella que apuntara al node 2 ara
+        delete _aux; //eliminem el node
+    }
 }
+//Si la llista no es buida, eliminem el node que apunta el rear.
 template <class Element> void LinkedDeque::deleteRear(){
-    //TODO
+    if(this->isEmpty())throw invalid_argument("La cua és buida.");
+    else{
+        Node<Element> *_aux; //node auxiliar que apuntarà al node que volem eliminar
+        _aux=this->_rear->getPrevious(); //assigmen el node auxiliar
+        _aux->getPrevious()->setNext(this->_rear);//movem la "fletxa" que apunta el node n al node n-1
+        this->_rear->setPrevious(_aux->getPrevious()); //movem la "fletxa" del sentinella que apuntara al node n-1 ara
+        delete _aux; //eliminem el node
+    }
 }
 template <class Element> void LinkedDeque::print(){
     //TODO
-}        
-template <class Element> const Element& LinkedDeque::getFront() const{
-    //TODO
 }
+//Comprovem que la cua no sigui buida i si no es dona el cas retornem l'element
+//del node on apunta el front. Recordem que hi ha el sentinella.
+template <class Element> const Element& LinkedDeque::getFront() const{
+    if(this->isEmpty())throw invalid_argument("La cua és buida.");
+    else{
+        return this->_front->getNext()->getElement();
+    }
+}
+//Comprovem que la cua no sigui buida i si no es dona el cas retornem l'element
+//del node on apunta el rear. Recordem que hi ha el sentinella.
 template <class Element> const Element& LinkedDeque::getRear() const{
-    //TODO
+    if(this->isEmpty())throw invalid_argument("La cua és buida.");
+    else{
+        return this->_rear->getPrevious()->getElement();
+    }
 }
 
 #endif /* LINKEDDEQUE_H */
