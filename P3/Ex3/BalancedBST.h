ddfa653ffd99983b4e24352b833db7d1cca2cb59
Gabriel Marin <gabriel.marin.sanchez.12@gmail.com>
2018-05-08 08:33:19 +0200

    Copia de coses

--- P3/Ex3/BalancedBST.h
+++ P3/Ex3/BalancedBST.h
@@ -90,6 +90,69 @@ template <class Type> BalancedBST<Type>::~BalancedBST(){
     cout << "Arbre eliminat" << endl;
 }
 
+//Destrueix un subarbre donada la seva arrel. Metode auxiliar per al destructor
+template <class Type> void BalancedBST<Type>::postDelete(NodeTree<Type>* p){
+    if(p!=nullptr){    
+        //primer eliminem el subarbre de l'esquerra
+        if(p->hasLeft()) this->postDelete(p->getLeft());
+        //despres eliminem al subarbre de la dreta
+        if(p->hasRight()) this->postDelete(p->getRight());
+        //per ultim eliminem el node en questio
+        delete p;
+    }
+}
+
+//Retorna el nombre de NodeTrees que hi ha a l'arbre.
+template <class Type> int BalancedBST<Type>::size() const{
+    return this->size(this->pRoot);
+}
+
+//Retorna el nombre de NodeTrees que hi ha en un subarbre donada la seva arrel.
+template <class Type> int BalancedBST<Type>::size(NodeTree<Type>* p) const{
+    //si el Node p es nul, retornem 0
+    if(p==nullptr) return 0;
+    //altrament, sabem que n'hi ha un i mirem els seus fills
+    else return 1 + size(p->getLeft()) + size(p->getRight());
+}
+
+//Retorna true si l'arbre està buit, false en cas contrari.
+template <class Type> bool BalancedBST<Type>::isEmpty() const{
+    return (this->pRoot == nullptr);
+}
+
+//Retorna l'adreça de l'arrel.
+template <class Type> NodeTree<Type>* BalancedBST<Type>::root() const{
+    return this->pRoot;
+}
+
+//Cerca un element. Retorna true si el troba, false en cas contrari.
+template <class Type> NodeTree<Type>* BalancedBST<Type>::search(int key) {
+    return this->search(pRoot,key);
+}
+
+//Cerca un element a partir d'un node. Quan el troba retorna el node.
+template <class Type> NodeTree<Type>* BalancedBST<Type>::search(NodeTree<Type>* p, int key) {
+    //si el node es null o el seu valor es el que busquem, el retornem
+    if(p==nullptr || p->getKey()==key)return p;
+    //si el seu valor es mes gran, busquem al fill de l'esquerra
+    else if(p->getKey()>key && p->hasLeft()){
+        return search(p->getLeft(),key);
+    }
+    //si no, com tindra un valor inferior, busquem al fill de la dreta
+    else if(p->hasRight()){
+        return search(p->getRight(),key);
+    }
+    return nullptr;//Si no ha estat agafat en un dels dos ifs vol dir que no existeix l'element
+}
+
+//Retorna un enter amb l'alçada d'un arbre, donada la seva arrel.
+template <class Type> int BalancedBST<Type>::getHeight(NodeTree<Type>* p){
+    //si el Node p es nul, retornem 0. L'alçada d'una fulla serà 1
+    if(p==nullptr)return 0;
+    //altrament, sabem que n'hi ha un i mirem els seus fills
+    else return 1 + max(getHeight(p->getLeft()),getHeight(p->getRight()));
+}
+
 #ifdef __cplusplus
 }
 #endif
